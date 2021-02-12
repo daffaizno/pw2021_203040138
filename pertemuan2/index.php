0069ba378a96ed2fe3c237caea4224bdb8dc294d
daffaizno <naufaldaffa@gmail.com>
2021-02-12 19:17:54 +0700

    menyelesaikan video pertemuan 2

---
+++ pertemuan2/index.php
@@ -0,0 +1,73 @@
+<?php
+/*  
+    Daffaiz Naufal  
+    203040138   
+    https://github.com/daffaizno/pw2021_203040138
+    Pertemuan 2 - 12 Februari 2021
+    Minggu ke-2 belajar mengenai sintaks dasar php
+*/
+
+// Standar output 
+// echo, print
+echo "daffaiz";
+print "daffaiz";
+// print_r
+print_r ("daffaiz");
+// // var_dump
+var_dump ("daffaiz");
+
+/* 
+    Penulisan Sintaks PHP :
+    1. PHP di dalam HTML
+    2. HTML di dalam PHP
+*/
+
+// Variabel dan Tipe Data 
+// Variabel -> tidak boleh diawali dengan angka, tetapi boleh mengandung angka contohnya: $nama1 bukan $1nama 
+// $nama = "daffaiz";
+// echo "Hallo, nama saya $nama";
+
+// Operator 
+
+// aritmatika
+// + - * / %
+// $x = 10;
+// $y = 20;
+// echo $x + $y;
+
+// penggabung string / concatenation / concat - > Operator penggabung string
+// . 
+// $nama_depan = "Daffaiz";
+// $nama_belakang = "Naufal";
+// echo $nama_depan . " " . $nama_belakang;
+
+// Assingment - > Operator Penugasan
+// =, +=, -=, *=, /=, %=, .=
+// $x = 1;
+// $x += 5;
+// echo $x;
+// $nama = "daffaiz";
+// $nama .= " ";
+// $nama .= "naufal";
+// echo $nama;
+
+// Perbandingan - > Hanya mengecek tipe data hanya nilainya saja
+// <, >, <=, >=, ==, !=
+// var_dump(3 < 1);
+// var_dump(3 > 1);
+
+// identitas 
+// ===, !==
+// var_dump(1 === "1");
+
+// Logika
+// &&, ||, !
+// $x = 20;
+// var_dump($x < 20 && $x % 2 == 0);
+// var_dump($x < 20 || $x % 2 == 0);
+
+?>
+
+
+
+    
